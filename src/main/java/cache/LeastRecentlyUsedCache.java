589cf9847fa79fe78199a98ed69e0c8d876ad461
alrscent <mihyang.sol.lee@gmail.com>
2026-02-27 06:58:13 +0900

    LRU Cache 구현

--- src/main/java/cache/LeastRecentlyUsedCache.java
+++ src/main/java/cache/LeastRecentlyUsedCache.java
@@ -1,6 +1,22 @@
 package cache;
 
+import cache.model.Item;
+
+import java.util.HashMap;
+import java.util.Map;
+
 //TODO sentinel(dummy) node(LRU에서 많이 쓴다고 함) 조사
 //TODO LRU 캐시의 요구사항 조사
 public class LeastRecentlyUsedCache {
+    Map<Long, Item> mapForCache = new HashMap<>();
+    public void put(Item item) {
+        mapForCache.put(item.getId(), item);
+    }
+    public Item get(long id) {
+        return mapForCache.get(id);
+    }
+
+    public int size() {
+        return mapForCache.size();
+    }
 }
--- src/test/java/cache/LeastRecentlyUsedCacheTest.java
+++ src/test/java/cache/LeastRecentlyUsedCacheTest.java
@@ -1,7 +1,7 @@
 package cache;
 
 import cache.model.Item;
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -20,33 +20,33 @@ public class LeastRecentlyUsedCacheTest {
 
     @Test
     @DisplayName("캐시 기본동작 - 값 저장 및 꺼내오기")
-    public void lruCacheTest1() {
+    public void lruCacheGetSetTest() {
         Item item = new Item(1L, "아이템");
         LeastRecentlyUsedCache cache = new LeastRecentlyUsedCache();
-        //TODO cache put/get/size 구현
-//        cache.put(item.getId(), item);
 
-//        Assertions.assertEquals(cache.size(), 1);
+        cache.put(item);
+        Assertions.assertThat(cache.size()).isEqualTo(1);
 
-//        Item itemByCache = cache.get(1L);
-//        Assertions.assertEquals(item, itemByCache);
+        Item itemByCache = cache.get(1L);
+        Assertions.assertThat(itemByCache).isEqualTo(item);
     }
 
     @Test
     @DisplayName("LRU 캐시 - 캐시 사이즈 초과시 오래된 항목이 제거되어야 한다")
-    public void lruCacheTest2() {
+    public void lruCacheRemoveTest() {
     }
 
     @Test
     @DisplayName("LRU 캐시 - 데이터 추가/갱신시 최근 사용한 것으로 순서가 변경되어야 한다")
-    public void lruCacheTest3() {}
+    public void lruCacheOrderChangeWhenAddedAndUpdatedTest() {}
 
     @Test
     @DisplayName("LRU 캐시 - 데이터를 읽었을 때 최근 사용한 것으로 순서가 변경되어야 한다")
-    public void lruCacheTest4() {}
+    public void lruCacheOrderChangeWhenReadTest() {}
 
     @Test
     @DisplayName("LRU 캐시 - 데이터를 조회했으나 캐시에 존재하지 않을 경우 DB에서 데이터를 조회 후 캐시에 데이터를 추가해야 한다")
-    public void lruCacheTest5() {}
+    public void lruCacheReadFailTest() {
+    }
 
 }
